9d2036d535b1aef888fa4a5b4791c1cab6d64d9b
Anastasiya Fatykhava <fatykhava@yandex.ru>
2021-02-28 21:22:06 +0300

    refactor: change sort  function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-	if (arguments.length === 0 || matrix.length === 0) return [];
+    if (arguments.length === 0 || matrix.length === 0) return [];
 
-	let arr = [];
-	arr.push(matrix[0]);
-	for (let i = 1; i < matrix.length; i++) {
-		if (i % 2 === 0) {arr.push(matrix[i]);}
-		else {arr.push(matrix[i].reverse());}
-	}
-	return arr.flat();
+    const mapFunc = (val, index) => {
+        if (index % 2 === 1) return val.reverse();
+        return val;
+    }
+
+    return matrix.map(mapFunc).flat();
+
 }
